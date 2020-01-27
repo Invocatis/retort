9444eda8d7f1ef807808b171c422ba3fa4896aa0
ljc237 <ljc237@cornell.edu>
2020-01-27 18:20:17 -0500

    Removed snapshot. Upgraded motif

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject retort "0.1.0-SNAPSHOT"
+(defproject retort "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/clojurescript "1.10.520"]
-                 [motif "0.1.0"]]
+                 [motif "1.0.1"]]
   :plugins [[lein-cljsbuild "1.1.4"]]
   :repl-options {:init-ns user}
   :cljsbuild {:builds {:main {:source-paths ["src"]
